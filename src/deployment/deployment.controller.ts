3056511a21d30fc85eb3576119814bb468d032d8
gre-ilya <ilya.grebenyuk.01@mail.ru>
2023-05-13 14:07:07 +0500

    Working on deployments entity...

--- src/deployment/deployment.controller.ts
+++ src/deployment/deployment.controller.ts
@@ -1,34 +1,66 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request} from '@nestjs/common';
 import { DeploymentService } from './deployment.service';
 import { CreateDeploymentDto } from './dto/create-deployment.dto';
 import { UpdateDeploymentDto } from './dto/update-deployment.dto';
+import {JwtAuthGuard} from "../auth/jwt-auth.guard";
+import {ApiBearerAuth, ApiCreatedResponse, ApiOkResponse, ApiParam, ApiTags} from "@nestjs/swagger";
+import {DeploymentEntity} from "./entities/deployment.entity";
 
 @Controller('projects/:project/services/:service')
+@ApiTags('deployments')
 export class DeploymentController {
   constructor(private readonly deploymentService: DeploymentService) {}
 
   @Post()
-  create(@Body() createDeploymentDto: CreateDeploymentDto) {
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiCreatedResponse({ type: DeploymentEntity })
+  @ApiParam({ name: 'service', required: true })
+  @ApiParam({ name: 'project', required: true })
+  create(@Request() req, @Param() params, @Body() createDeploymentDto: CreateDeploymentDto) {
     return this.deploymentService.create(createDeploymentDto);
   }
 
   @Get()
-  findAll() {
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOkResponse({ type: [DeploymentEntity] })
+  @ApiParam({ name: 'service', required: true })
+  @ApiParam({ name: 'project', required: true })
+  findAll(@Request() req, @Param() params) {
     return this.deploymentService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(':deployment')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOkResponse({ type: DeploymentEntity })
+  @ApiParam({ name: 'deployment', required: true })
+  @ApiParam({ name: 'service', required: true })
+  @ApiParam({ name: 'project', required: true })
+  findOne(@Request() req, @Param() params) {
     return this.deploymentService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDeploymentDto: UpdateDeploymentDto) {
+  @Patch(':deployment')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOkResponse({ type: DeploymentEntity })
+  @ApiParam({ name: 'deployment', required: true })
+  @ApiParam({ name: 'service', required: true })
+  @ApiParam({ name: 'project', required: true })
+  update(@Request() req, @Param() params, @Body() updateDeploymentDto: UpdateDeploymentDto) {
     return this.deploymentService.update(+id, updateDeploymentDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(':deployment')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOkResponse({ type: DeploymentEntity })
+  @ApiParam({ name: 'deployment', required: true })
+  @ApiParam({ name: 'service', required: true })
+  @ApiParam({ name: 'project', required: true })
+  remove(@Request() req, @Param() params) {
     return this.deploymentService.remove(+id);
   }
 }
